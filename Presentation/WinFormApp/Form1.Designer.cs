649a20c106e4920b358deae34df1dd85d648e4d6
Usman <usman.solemn@gmail.com>
2022-08-14 14:21:39 +0500

    init

---
+++ Presentation/WinFormApp/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ImageListDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList = new System.Windows.Forms.ListView();
+            this.selectedImage = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previousBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.saveAsBtn = new System.Windows.Forms.Button();
+            this.selectDirectoryBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.imageList.HideSelection = false;
+            this.imageList.Location = new System.Drawing.Point(13, 42);
+            this.imageList.Margin = new System.Windows.Forms.Padding(4);
+            this.imageList.Name = "imageList";
+            this.imageList.Size = new System.Drawing.Size(225, 505);
+            this.imageList.TabIndex = 0;
+            this.imageList.UseCompatibleStateImageBehavior = false;
+            this.imageList.Visible = false;
+            this.imageList.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.imageList_ItemSelectionChanged);
+            // 
+            // selectedImage
+            // 
+            this.selectedImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectedImage.Location = new System.Drawing.Point(267, 42);
+            this.selectedImage.Margin = new System.Windows.Forms.Padding(4);
+            this.selectedImage.Name = "selectedImage";
+            this.selectedImage.Size = new System.Drawing.Size(604, 430);
+            this.selectedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.selectedImage.TabIndex = 1;
+            this.selectedImage.TabStop = false;
+            this.selectedImage.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(884, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Visible = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectDirectoryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // selectDirectoryToolStripMenuItem
+            // 
+            this.selectDirectoryToolStripMenuItem.Name = "selectDirectoryToolStripMenuItem";
+            this.selectDirectoryToolStripMenuItem.Size = new System.Drawing.Size(197, 26);
+            this.selectDirectoryToolStripMenuItem.Text = "Select Directory";
+            this.selectDirectoryToolStripMenuItem.Click += new System.EventHandler(this.selectDirectoryToolStripMenuItem_Click);
+            // 
+            // previousBtn
+            // 
+            this.previousBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.previousBtn.Location = new System.Drawing.Point(264, 488);
+            this.previousBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.previousBtn.Name = "previousBtn";
+            this.previousBtn.Size = new System.Drawing.Size(83, 50);
+            this.previousBtn.TabIndex = 3;
+            this.previousBtn.Text = "Previous";
+            this.previousBtn.UseVisualStyleBackColor = true;
+            this.previousBtn.Visible = false;
+            this.previousBtn.Click += new System.EventHandler(this.button_navigation);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nextBtn.Location = new System.Drawing.Point(378, 488);
+            this.nextBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(83, 50);
+            this.nextBtn.TabIndex = 4;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Visible = false;
+            this.nextBtn.Click += new System.EventHandler(this.button_navigation);
+            // 
+            // saveAsBtn
+            // 
+            this.saveAsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveAsBtn.Location = new System.Drawing.Point(730, 489);
+            this.saveAsBtn.Name = "saveAsBtn";
+            this.saveAsBtn.Size = new System.Drawing.Size(142, 49);
+            this.saveAsBtn.TabIndex = 5;
+            this.saveAsBtn.Text = "Save As Png";
+            this.saveAsBtn.UseVisualStyleBackColor = true;
+            this.saveAsBtn.Visible = false;
+            this.saveAsBtn.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // selectDirectoryBtn
+            // 
+            this.selectDirectoryBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.selectDirectoryBtn.Location = new System.Drawing.Point(378, 226);
+            this.selectDirectoryBtn.Name = "selectDirectoryBtn";
+            this.selectDirectoryBtn.Size = new System.Drawing.Size(163, 48);
+            this.selectDirectoryBtn.TabIndex = 6;
+            this.selectDirectoryBtn.Text = "Select A Directory";
+            this.selectDirectoryBtn.UseVisualStyleBackColor = true;
+            this.selectDirectoryBtn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 554);
+            this.Controls.Add(this.selectDirectoryBtn);
+            this.Controls.Add(this.saveAsBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.previousBtn);
+            this.Controls.Add(this.selectedImage);
+            this.Controls.Add(this.imageList);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView imageList;
+        private System.Windows.Forms.PictureBox selectedImage;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectDirectoryToolStripMenuItem;
+        private System.Windows.Forms.Button previousBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button saveAsBtn;
+        private System.Windows.Forms.Button selectDirectoryBtn;
+    }
+}
+
